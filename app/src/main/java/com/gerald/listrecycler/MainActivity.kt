8c49dc2fb9faab66843a5023b3e98c97d486785f
Gerii254 <geraldlazaro427@gmail.com>
2022-07-19 22:41:59 +0300

    List of People and Contacts

---
+++ app/src/main/java/com/gerald/listrecycler/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.gerald.listrecycler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import io.bloco.faker.Faker
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val faker = Faker()
+        var peopleList = arrayListOf<Person>()
+
+        for (i in 1..500) {
+            val name = faker.name.firstName() + "" + faker.name.lastName()
+            val email = faker.internet.email()
+            val address = faker.address.streetAddress()
+            val age = faker.number.between(18, 75)
+            val university = faker.university.name()
+            val phone = faker.phoneNumber.phoneNumber()
+           // Log.d("FAKER", "$name : $email :$address : $age : $university")//debugging
+            val person = Person(name, email, address, age, phone)
+            peopleList.add(person)
+        }
+       // peopleList.forEach{Log.d("PEOPLE",it.name)}
+
+        val RecyclerPeople : RecyclerView = findViewById(R.id.recyclerViewPeople)
+        RecyclerPeople.layoutManager = LinearLayoutManager(this)
+
+        val adapter = CustomAdapter(peopleList)
+        RecyclerPeople.adapter = adapter
+
+
+    }
+}
---
+++ app/src/main/java/com/gerald/listrecycler/Person.kt
@@ -0,0 +1,13 @@
+package com.gerald.listrecycler
+
+import android.provider.ContactsContract
+import io.bloco.faker.components.University
+import java.net.Inet4Address
+
+ data class Person(
+    val name: String,
+    val email: String,
+    val address: String,
+    val age: Int,
+    val phone: String
+)
